f42df31241acfbc7d8ddddc680c79df389be0c4e
SvetaSarkisova <sarkisova0310@gmail.com>
2020-06-16 21:58:15 -0700

    Initial commit

---
+++ test/specs/basic.js
@@ -0,0 +1,25 @@
+describe ( 'Main page', ()=> {
+    it('should title= Progress Monitor', function () {
+       browser.url('https://app.pasv.us/')
+       const Title = $("//span[@id='site-name']").getText()
+        expect(Title).toEqual('Progress Monitor')
+    });
+
+    it('verify that Progress Monitor is clickable ', function () {
+       const result =  $("//span[@id='site-name']").isClickable();
+        expect(result).toEqual(true);
+
+    });
+
+    it('should verify Login button is present', function () {
+        const button = $("//a[@qa='login-link']").isDisplayed()
+        expect(button).toEqual(true)
+
+    });
+
+    it('should verify Login Button has text = Login', function () {
+        const loginButton = $("//a[@qa='login-link']").getText();
+            expect(loginButton).toEqual('Login')
+
+    });
+});
